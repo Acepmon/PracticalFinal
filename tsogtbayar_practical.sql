3cbf48da77ae6ffb421dffcef98df349d22733de
D. Tsogtbayar <dTsogtbayar123@gmail.com>
2015-08-05 09:35:03 +0900

    Forgot to add database

--- tsogtbayar_practical.sql
+++ tsogtbayar_practical.sql
@@ -7,6 +7,9 @@
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
+create database tsogtbayar_practical;
+use tsogtbayar_practical;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
